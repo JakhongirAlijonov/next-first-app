328a21f31ac7ab5397b1265cd4929d9d520e59f6
JakhongirAlijonov <jakhongirweb@gmail.com>
2023-09-08 20:28:56 +0500

    some ts files added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,34 @@
+import { PostType } from "../interface/type"
 
-export default function BlogPage() {
-  return (
-    <div>BlogPage</div>
-  )
+const  getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+ 
+  
 }
+
+export default async function BlogPage() {
+
+  const data : PostType[] = await getData()
+  
+  return (
+    <>
+      {
+        data.map(post=>{
+          return (
+            <div key={post.id}>
+              <p>{post.id}</p>
+              <h2>{post.title}</h2>
+            </div>
+          )
+        })
+      }
+      
+    </>
+    )
+  }
+
